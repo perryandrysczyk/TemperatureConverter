19073caf46ff642c5fc7b7a8cd8ca2c5870a3deb
perryandrysczyk <47957840+perryandrysczyk@users.noreply.github.com>
2019-03-10 14:11:06 -0400

    added program

---
+++ TempConverter/TempConverter/TempConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Ask user for input
+			Console.WriteLine("What is the temperature in fahrenheit?");
+			//read fahrenheit input
+			string temp = Console.ReadLine();
+			int x = Int32.Parse(temp);
+			//fahrenheit to Celsius conversion
+			int y = x - 32;
+			double z = y * 0.5556;
+			//print out whether it's hot or cold
+			Console.WriteLine("It is " + z + " degrees Celsius!");
+			if (z > 23)
+			{
+				Console.WriteLine("That's hot!");
+			}
+
+			else if (z <= 12)
+			{
+				Console.WriteLine("That's cold!");
+			}
+			else if (z > 12 || z <= 23)
+			{
+				Console.WriteLine("Such a nice day!");
+			}
+
+			//Wait for user to exit console
+			Console.ReadKey();
+
+
+		}
+	}
+}
